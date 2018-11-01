ced5a9428623d29924598323b724f16fa0a89a27
Casey Corcoran <github@snaptopixel.com>
2018-11-01 12:57:22 -0400

    Stuff

--- stencil.config.ts
+++ stencil.config.ts
@@ -3,5 +3,13 @@ import { Config } from '@stencil/core';
 // https://stenciljs.com/docs/config
 
 export const config: Config = {
-  globalStyle: 'src/global/app.css'
+  globalStyle: 'src/global/app.css',
+  outputTargets: [
+    {
+      type: 'www',
+      baseUrl: '/snaptopixel/stencil-test',
+      resourcesUrl: '/snaptopixel/stencil-test/build/app',
+      dir: 'docs'
+    }
+  ]
 };
